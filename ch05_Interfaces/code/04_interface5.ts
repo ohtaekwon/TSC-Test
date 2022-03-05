fe327f41f0f9b4e90e0e650c53e1438821be5a3f
ohtaekwon <ohtk1119@gmail.com>
2022-03-05 20:48:57 +0900

    interface

--- ch05_Interfaces/code/04_interface5.ts
+++ ch05_Interfaces/code/04_interface5.ts
@@ -1,6 +1,22 @@
 interface Iperson1{
   name:string;
+  // optional
   age?:number;
   hello():void;
 }
 
+class Person implements Iperson1 {
+  name: string;
+  age?: number | undefined;
+
+  constructor(name:string){
+    this.name=name
+  }
+  
+  hello(): void {
+    console.log(`안녕하세요! ${this.name} 입니다.`)
+  }   
+}
+
+const person04 = new Person('Mark');
+person04.hello();
